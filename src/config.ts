84ce9a60ae0fa27ee6a6da09ac77946ebc6337c4
Sergei Kultyshkin <kultyshkin2003@gmail.com>
2025-12-05 07:48:25 +0300

    add(main): добавили базовый инит проект и простую авторизацию через expressBasicAuth

---
+++ src/config.ts
@@ -0,0 +1,2 @@
+export const ADMIN_LOGIN = process.env.ADMIN_LOGIN ?? 'admin';
+export const ADMIN_PASSWORD = process.env.ADMIN_LOGIN ?? 'admin';
---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { NestFactory } from '@nestjs/core';
+import * as expressBasicAuth from 'express-basic-auth';
+import { AppModule } from './app.module';
+import { ADMIN_LOGIN, ADMIN_PASSWORD } from './config';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.use(expressBasicAuth({ users: { [ADMIN_LOGIN]: ADMIN_PASSWORD } }));
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap().catch((error) => console.error(error));
