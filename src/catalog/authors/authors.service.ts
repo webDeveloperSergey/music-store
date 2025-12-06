95176d140e97500001647a166f8c2fca005e3b4c
Sergei Kultyshkin <kultyshkin2003@gmail.com>
2025-12-06 19:47:16 +0300

    add(main): добавили checkName, улучшили create и добавили update

--- src/catalog/authors/authors.service.ts
+++ src/catalog/authors/authors.service.ts
@@ -1,4 +1,8 @@
-import { Injectable, NotImplementedException } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotImplementedException,
+} from '@nestjs/common';
 import { randomUUID } from 'crypto';
 import { PrismaService } from 'src/prisma';
 import { CreateAuthorDTO, ReadAuthorDTO, ReadManyQueryAuthorDTO } from './dto';
@@ -26,6 +30,8 @@ export class AuthorsService {
   }
 
   async create(data: CreateAuthorDTO): Promise<string> {
+    await this.checkName(data.name);
+
     const { id } = await this.prisma.author.create({
       data: { id: randomUUID(), ...data },
     });
@@ -33,12 +39,32 @@ export class AuthorsService {
     return id;
   }
 
-  update(authorId: string, data: CreateAuthorDTO): Promise<ReadAuthorDTO> {
-    console.log(data);
-    throw new NotImplementedException(`Method not implemented ${authorId}`);
+  async update(authorId: string, data: CreateAuthorDTO): Promise<void> {
+    await this.checkName(data.name, authorId);
+
+    await this.prisma.author.update({
+      where: { id: authorId },
+      data,
+    });
   }
 
   delete(authorId: string): Promise<void> {
     throw new NotImplementedException(`Method not implemented ${authorId}`);
+  }
+
+  private async checkName(name: string, authorId?: string): Promise<void> {
+    const id = authorId ? { not: authorId } : undefined;
+
+    const existingAuthor = await this.prisma.author.findFirst({
+      // equals - точное совпадение в БД
+      // insensitive - означает поиск без учета регистра
+      where: { name: { equals: name, mode: 'insensitive' }, id },
+    });
+
+    if (existingAuthor) {
+      // ConflictException - статусом 409 (Conflict)
+      // Пользователь пытается создать автора с именем, которое уже существует
+      throw new ConflictException(`Author with name '${name}' already exists.`);
+    }
   }
 }
